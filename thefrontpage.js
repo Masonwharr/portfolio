809f7358c9588ba14286d3fbff6ba20c15a8f482
masonwharr <mason.harr1@aol.com>
2017-02-25 03:16:43 -0600

    Add files via upload

---
+++ thefrontpage.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	var windowSize = $(window).width();
+	var logo = document.getElementById('icon');
+	 var link1 = document.getElementsByClassName('a1');
+	 var portfolio = document.getElementsByClassName('a2');
+});
